c968d307bb7b8777cf7c2eff9e647311e3126ce8
mehdizebhi <mehdizebhi@yahoo.com>
2024-03-10 19:29:45 +0330

    add / add client credential dto model

---
+++ src/main/kotlin/model/ClientCredential.kt
@@ -0,0 +1,14 @@
+package model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ClientCredential(
+    @SerialName("access_token")
+    val accessToken: String,
+    @SerialName("expires_in")
+    val expiresIn: Int,
+    @SerialName("token_type")
+    val tokenType: String
+)
