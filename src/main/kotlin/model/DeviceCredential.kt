ee39713a99d407f84a0a77b9038301379b21dcd1
mehdizebhi <mehdizebhi@yahoo.com>
2024-03-10 19:51:49 +0330

    add / add device credential dto model

---
+++ src/main/kotlin/model/DeviceCredential.kt
@@ -0,0 +1,19 @@
+package model
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DeviceCredential(
+    @SerialName("device_code")
+    val deviceCode: String,
+    @SerialName("expires_in")
+    val expiresIn: Int,
+    @SerialName("interval")
+    val interval: Int,
+    @SerialName("user_code")
+    val userCode: String,
+    @SerialName("verification_uri")
+    val verificationUri: String
+)
