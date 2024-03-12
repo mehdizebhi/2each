d5a82ffc6696f10687f3fe0337a06518f8578c51
mehdizebhi <mehdizebhi@yahoo.com>
2024-03-12 18:36:33 +0330

    feature / add user access token flow unit test

--- src/test/kotlin/api/AuthenticationClientTest.kt
+++ src/test/kotlin/api/AuthenticationClientTest.kt
@@ -1,6 +1,9 @@
 package api
 
 import kotlinx.coroutines.runBlocking
+import util.CryptoUtils
+import java.nio.file.Files
+import java.nio.file.Path
 import kotlin.test.Test
 
 class AuthenticationClientTest {
@@ -15,5 +18,15 @@ class AuthenticationClientTest {
     fun deviceCodeGrantFlow() {
         val code = runBlocking { AuthenticationClient.deviceCodeGrantFlow() }
         println("Your Device Code is: $code")
+    }
+
+    @Test
+    fun userAccessTokenFlow() {
+        val token = runBlocking { AuthenticationClient.fetchOAuthTokenByDeviceCode("deviceCode") }
+
+        val encryptedAccessToken = CryptoUtils.encrypt(token.accessToken)
+        val encryptedRefreshToken = CryptoUtils.encrypt(token.accessToken)
+        val tokenPairString = "$encryptedAccessToken\n$encryptedRefreshToken"
+        Files.write(Path.of("tokenpairTest.txt"), tokenPairString.toByteArray())
     }
 }
